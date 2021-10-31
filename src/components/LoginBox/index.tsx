b8e894347eda3f7200cadf78f2e93e89a26e125a
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2021-10-30 22:27:00 -0300

    📝 :pencil: [Minor] Send message

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,12 +1,17 @@
 import styles from './styles.module.scss'
 import { VscGithubInverted } from 'react-icons/vsc'
 import logoImg from '../../assets/logo.svg'
+import { useContext, useEffect } from 'react'
+import { api } from '../../services/api'
+import { AuthContext } from '../../contexts/auth'
 
 export function LoginBox() {
+    const { signInUrl } = useContext(AuthContext)
+    
     return (
         <div className={styles.loginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
-            <a href="#" className={styles.signInWithGithub}>
+            <a href={signInUrl} className={styles.signInWithGithub}>
                 <VscGithubInverted size="24" />
                 Entrar com Github
             </a>
